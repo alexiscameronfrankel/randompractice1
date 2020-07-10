a8e1a78564760a8e7a4083e5a50e12fddd44940a
Alexis <53586057+alexiscameronfrankel@users.noreply.github.com>
2020-07-10 16:48:34 -0400

    Create stringrepeat.js

---
+++ stringrepeat.js
@@ -0,0 +1,13 @@
+// Write a function called repeatString which repeats the given String src exactly count times.
+
+function repeatStr (n, s) {
+  let i = 0 
+  let newThing = ''
+  
+  
+  for(i; i < n; i++){
+    newThing += s
+  }
+  
+  return newThing
+}
