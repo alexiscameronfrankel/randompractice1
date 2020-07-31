0f566cf5e1f469553a73a3c21952b8c96e99c369
Alexis <53586057+alexiscameronfrankel@users.noreply.github.com>
2020-07-30 20:00:33 -0400

    Update needone.js

--- needone.js
+++ needone.js
@@ -1,8 +1,6 @@
 /* You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not.  */
 
-Array can contain numbers or strings. X can be either. */
-
-Return true if the array contains the value, false if not.
 function check(a, x) {
   let i = 0 
   let z = a.indexOf(x)
